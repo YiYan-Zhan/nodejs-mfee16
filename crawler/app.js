5c7848577f08bea5c4b96ff55a87bfe595c2952e
Vincent Jan <yiyanzhan0823@gmail.com>
2021-05-30 01:21:49 +0800

    app.js revised

--- crawler/app.js
+++ crawler/app.js
@@ -1,15 +1,12 @@
 let url="https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=2610";
 
-
+let url2="https://www.twse.com.tw/exchangeReport/STOCK_DAY";
 
 
 const axios= require('axios');
 
-axios.get(url,{
-    params:{
-        ID: 12345
-    }
-}).then(function(response){
+// Method 1: full url, no needs any params content
+axios.get(url).then(function(response){
     // console.log(response);
     // console.log(response.data);
     console.log(response.data.title);
@@ -18,3 +15,15 @@ axios.get(url,{
 }).catch(function(error){
     console.log(error);
 })
+
+// Method 2: simple url, controled by params content 
+axios.get(url2,{
+    params:{
+        response: "json",
+        date: "20210523",
+        stockNo: "2610"
+    }
+}).then(function(response){
+    console.log(response.data.title);
+    console.log(response.data.data);
+});
