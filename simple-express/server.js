045cc700a4a3fea1b4c0b27d6ff839c6d941b410
Vincent Jan <yiyanzhan0823@gmail.com>
2021-06-18 16:00:34 +0800

    views pages created

--- simple-express/server.js
+++ simple-express/server.js
@@ -5,6 +5,12 @@ let app = express();
 
 app.use(express.static("public"));
 
+//
+app.set("views", "views");
+
+//告訴express對應位置
+app.set("view engine", "pug");
+
 app.use(function (req, res, next) {
     console.log("connected");
     next();
@@ -13,11 +19,16 @@ app.use(function (req, res, next) {
 // router
 
 app.get("/", function (req, res) {
-    res.send("Hi express");
+    // res.send("Hi express");
+
+    // 將首頁指向指定檔案
+    res.render("index");
 });
 
 app.get("/aboutus", function (req, res) {
-    res.send("About us");
+    // res.send("About us");
+
+    res.render("aboutUs");
 });
 
 app.listen(3000, function () {
